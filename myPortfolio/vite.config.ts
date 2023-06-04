8e20ac3ab78496f5d7edf1e376489d0e99faf046
Lee Sijun <season2lee@gmail.com>
2023-06-04 23:30:47 +0900

    check

---
+++ myPortfolio/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/PORTFOLIO/", // 이 부분을 수정
+});
