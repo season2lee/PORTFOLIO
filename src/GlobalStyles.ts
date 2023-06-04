1e82499328575b02c3ed9b824add8a15e838b830
Lee Sijun <season2lee@gmail.com>
2023-06-05 02:15:42 +0900

    check

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    line-height: 1.5;
+    color: navy;
+  }
+
+  h2, p {
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+function AboutPage() {
+  return <div>SeasonLee adf</div>;
+}
+
+export default AboutPage;
