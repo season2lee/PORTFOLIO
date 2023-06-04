01e8ae4c77a793731e3dec9e0f40ab49ae02126c
Lee Sijun <season2lee@gmail.com>
2023-06-05 00:10:14 +0900

    why,,,

--- myPortfolio/vite.config.ts
+++ myPortfolio/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PORTFOLIO/", // 이 부분을 수정
+  base: "/myPortfolio/", // 이 부분을 수정
 });
