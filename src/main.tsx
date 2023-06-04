4f5042c696667193853e9bc7efc2caeaa3d685c7
Lee Sijun <season2lee@gmail.com>
2023-06-05 03:01:45 +0900

    asdf

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/PORTFOLIO/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
